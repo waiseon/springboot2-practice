70be1c7dce631601f44e7301b3519063eb946d56
waiseon <marco39873@163.com>
2021-03-05 19:13:40 +0800

    init

--- src/main/java/com/wilson/Springboot2Application.java
+++ src/main/java/com/wilson/Springboot2Application.java
@@ -1,4 +1,4 @@
-package com.wilson.demo;
+package com.wilson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/wilson/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.wilson.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/hello")
+    public String hello(){
+        return "Hello, Spring Boot 2!";
+    }
+}
